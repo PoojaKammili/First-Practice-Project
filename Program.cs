1f0e18165ca9fb007e8ecff0130ea5ab621ddc60
PoojaKammili <kammilipooja235@gmail.com>
2025-10-18 15:37:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitDemoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Pooja! 🎉");
+            Console.WriteLine("This is your Git + GitHub + Visual Studio learning demo!");
+            Console.WriteLine("Enter your favorite programming language:");
+            string lang = Console.ReadLine();
+            Console.WriteLine($"You love {lang}! That's awesome 💻");
+        }
+    }
+}
