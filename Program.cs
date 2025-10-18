4058c28d1d30d9dce6230af2077e16868fd02193
PoojaKammili <kammilipooja235@gmail.com>
2025-10-18 16:00:17 +0530

    Fix typo in modification message

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace GitDemoApp
             Console.WriteLine("Enter your favorite programming language:");
             string lang = Console.ReadLine();
             Console.WriteLine($"You love {lang}! That's awesome 💻");
-            Console.WriteLine($"This is the new modification");
+            Console.WriteLine($"This is the new modificationin github");
         }
     }
 }
